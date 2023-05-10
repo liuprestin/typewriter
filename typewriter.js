79244547d30b57fc6f072a2b366eaed3b63b1e82
Prestin Liu <liuprestin@gmail.com>
2023-05-09 21:29:14 -0700

    implemented the animation

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,20 @@
-const sentence = "hello there from lighthouse labs";
+const sentence_test = "hello there from lighthouse labs";
 
-setTimeout(() => {
-    for (const char of sentence) {
-        process.stdout.write(char);
-    }
-  }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
+
+//given a string - animate as if it was being typed
+//here we take advantage of timing delay.
+function animatedTyping(sentence){
+
+for (let i = 0; i<sentence.length; i++){
+    
+    setTimeout(() => {
+    
+        process.stdout.write(sentence.charAt(i));
+    
+  }, i*200); // <= 1s delay to make it noticeable. Can dial it down later.
+}
+}
+
+//TEST
+
+animatedTyping(sentence_test);
